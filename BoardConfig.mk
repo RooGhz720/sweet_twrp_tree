f85172fd2ac85e5e0feb7c3926a013cd63bc3af1
RooGhz720 <rooghz720@gmail.com>
2023-03-01 19:57:00 +0800

    recovery: change thermal info
    Signed-off-by: RooGhz720 <rooghz720@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
-TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone18/temp
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone80/temp
 
 # Crypto
 BOARD_USES_QCOM_FBE_DECRYPTION := true
